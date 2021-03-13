d15bed4acd2e5ec6c0dff778b12848076e3ef3c6
margaret-scheers <margaret.scheers@pobox.com>
2021-03-13 09:40:46 -0600

    add actuator

--- src/main/java/com/in28minutes/spring/basics/springbootin10steps/SpringbootIn10StepsApplication.java
+++ src/main/java/com/in28minutes/spring/basics/springbootin10steps/SpringbootIn10StepsApplication.java
@@ -2,13 +2,19 @@ package com.in28minutes.spring.basics.springbootin10steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class SpringbootIn10StepsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+
+        for (String name : applicationContext.getBeanDefinitionNames()) {
+            System.out.println(name);
+
+        }
     }
 
 }
